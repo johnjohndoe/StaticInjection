a09292eda65ac22f53e37a848882d69f59a6c425
Tobias Preuss <tobias.preuss@googlemail.com>
2017-06-12 10:57:15 +0200

    Still fail in @AfterInject.

--- app/src/main/java/com/example/staticinjection/MainActivity.java
+++ app/src/main/java/com/example/staticinjection/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.staticinjection;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import org.androidannotations.annotations.AfterInject;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		LogUtil.log(this, "ONCREATE");
+
+		// Fails
+		// LogUtil.log(this, "ONCREATE");
+	}
+
+	@AfterInject
+	public void afterInject() {
+		LogUtil.log(this, "AFTERINJECT");
 	}
 
 }
